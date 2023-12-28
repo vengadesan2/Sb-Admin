ded38a03ce031330381ea1a9b5b06c374e354256
vengadesan2 <vengadsanguna24@gmail.com>
2023-12-28 22:00:51 +0530

    first

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import React,{useState} from 'react'
+import Sidebar from './components/Sidebar'
+import Dashboard from './components/Dashboard'
+import {BrowserRouter,Routes, Route, Navigate} from 'react-router-dom'
+import AddUser from './components/AddUser'
+import EditUser from './components/EditUser'
+
+function App() {
+  let [user,setUser] = useState([
+    {
+      id:1,
+      name:"Naga",
+      email:"naga@gmail.com",
+      mobile:"987654321",
+      batch:"B53"
+    },
+    {
+      id:2,
+      name:"Abimani",
+      email:"abimani@gmail.com",
+      mobile:"78987654567",
+      batch:"B53"
+    },
+    {
+      id:3,
+      name:"Amsa",
+      email:"amsa@gmail.com",
+      mobile:"89876543456",
+      batch:"B53"
+    }
+  ])
+  return <div id="wrapper">
+    
+    <BrowserRouter>
+    <Sidebar/>
+      <Routes>
+        <Route path='/dashboard' element={<Dashboard user={user} setUser={setUser}/>}/>
+        <Route path='/add-user' element={<AddUser user={user} setUser={setUser}/>}/>
+        <Route path='/edit-user/:id' element={<EditUser user={user} setUser={setUser}/>}/>
+        <Route path='*' element={<Navigate to='/dashboard'/>}/>
+      </Routes>
+    </BrowserRouter>
+  </div>
+}
+
+export default App
---
+++ src/components/Dashboard.jsx
@@ -0,0 +1,99 @@
+import React from 'react'
+import Table from 'react-bootstrap/Table';
+import Button from 'react-bootstrap/Button';
+import { useNavigate } from 'react-router-dom';
+function Dashboard({user,setUser}) {
+    
+    let data = [
+        {
+            title:"EARNINGS (MONTHLY)",
+            value:"$45,000",
+            color:'primary',
+            icon:'fa-calendar',
+            isProgress:false
+        },
+        {
+            title:"EARNINGS (ANNUAL)",
+            value:"$215,000",
+            color:'success',
+            icon:'fa-dollar-sign',
+            isProgress:false
+        },
+        {
+            title:"TASKS",
+            value:"20",
+            color:'info',
+            icon:'fa-clipboard-list',
+            isProgress:true
+        },
+        {
+            title:"PENDING REQUEST",
+            value:"18",
+            color:'warning',
+            icon:'fa-comments',
+            isProgress:false
+        }
+    ]
+    let navigate = useNavigate()
+
+    let handleDelete = (id)=>{
+        let index;
+        for(let i = 0;i<user.length;i++)
+        {
+            if(user[i].id==id)
+            {
+                index = i
+                break;
+            }
+        }
+        let newArray = [...user]//deep copy or Immutable
+        newArray.splice(index,1)
+        setUser(newArray)
+    }
+  return <>
+    <div id="content-wrapper" className="d-flex flex-column">
+        <div id="content">
+            <div className="container-fluid">
+                <div className="d-sm-flex align-items-center justify-content-between mb-4">
+                    <h1 className="h3 mb-0 text-gray-800">Dashboard</h1>
+                </div>
+              
+                <div className="row">
+                <Table striped bordered hover>
+                    <thead>
+                        <tr>
+                        <th>#</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Mobile</th>
+                        <th>Batch</th>
+                        <th>Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                       {
+                            user.map((e,i)=>{
+                                return <tr key={e.id}>
+                                    <td>{e.id}</td>
+                                    <td>{e.name}</td>
+                                    <td>{e.email}</td>
+                                    <td>{e.mobile}</td>
+                                    <td>{e.batch}</td>
+                                    <td>
+                                        <Button variant='primary' onClick={()=>navigate(`/edit-user/${e.id}`)}>Edit</Button>
+                                        &nbsp;
+                                        <Button variant='danger' onClick={()=>{handleDelete(e.id)}}>Delete</Button>
+                                    </td>
+                                </tr>
+                            })
+                       }
+                    </tbody>
+                </Table>
+                </div>
+            </div>
+        </div>
+    </div>
+  </>
+}
+
+export default Dashboard
